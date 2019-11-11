fbb68ffad1911b07d50df0e3de6aadd602bb4316
Alberto Becerra <albertobf@gmail.com>
2019-11-11 18:34:19 +0000

    PriceSupermarket should be deleted if any of its parents is deleted.

--- app/src/main/java/com/github/albertobf/grocerycompanion/model/PriceSupermarket.kt
+++ app/src/main/java/com/github/albertobf/grocerycompanion/model/PriceSupermarket.kt
@@ -3,6 +3,7 @@ package com.github.albertobf.grocerycompanion.model
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.ForeignKey
+import androidx.room.ForeignKey.CASCADE
 import java.math.BigDecimal
 
 @Entity(tableName = "price_supermarket",
@@ -10,20 +11,23 @@ import java.math.BigDecimal
     foreignKeys = [
         ForeignKey(entity = Product::class,
             parentColumns = ["id"],
-            childColumns = ["product_id"]),
+            childColumns = ["product_id"],
+            onDelete = CASCADE),
         ForeignKey(entity = Supermarket::class,
             parentColumns = ["id"],
-            childColumns = ["supermarket_id"]),
+            childColumns = ["supermarket_id"],
+            onDelete = CASCADE),
         ForeignKey(entity = Currency::class,
             parentColumns = ["id"],
-            childColumns = ["currency_id"])
+            childColumns = ["currency_id"],
+            onDelete = CASCADE)
 ])
 data class PriceSupermarket(
     @ColumnInfo(name = "product_id")
     val productId: Long,
     @ColumnInfo(name = "supermarket_id")
     val supermarketId: Long,
-    val price: BigDecimal,
+    var price: BigDecimal,
     @ColumnInfo(name = "currency_id")
     val currencyId: Long
 )
