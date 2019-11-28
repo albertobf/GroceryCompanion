e2e745283f647024c38e957ee37e653318019018
Alberto Becerra <albertobf@gmail.com>
2019-11-28 14:47:05 +0000

    Setting up action bar on nav controller.

--- app/src/main/java/com/github/albertobf/grocerycompanion/ui/MainActivity.kt
+++ app/src/main/java/com/github/albertobf/grocerycompanion/ui/MainActivity.kt
@@ -2,6 +2,8 @@ package com.github.albertobf.grocerycompanion.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.github.albertobf.grocerycompanion.R
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +11,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val navController = findNavController(R.id.nav_host_fragment)
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment)
+        return navController.navigateUp()
     }
 }
